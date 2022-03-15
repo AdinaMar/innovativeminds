d38287f73ac4585c67744e08519e5dd8dec7a8ba
adrianabecica <becica.adrianamihaela@gmail.com>
2022-03-15 23:35:35 +0200

    Button LogOut

--- src/components/Users/UserCard.js
+++ src/components/Users/UserCard.js
@@ -1,23 +1,60 @@
 import React, { useState } from 'react'
 import {GiHamburgerMenu} from 'react-icons/gi';
 import user from '../../images/userIcon.jpg';
+import photo from '../../images/camera.png';
 import UserActivities from './UserActivities';
 import { Link } from 'react-router-dom';
+import $ from 'jquery'; 
 
 const UserCard = () => {
 
+  {/*$(document).ready(function() {
+
+    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+               
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    });
+});
+*/}
 
   return (
     <div> 
 <div className='card-wrapper'>
  
 
-  
+{/*<div class="circle">
+       
+       <img class="profile-pic" src={user} alt="user-pic"/>
+     </div>
+     <div class="p-image">
+       
+     <i class="fa fa-camera upload-button"><img class="photo-pic" src={photo} alt="user-pic"/></i>
+        <input class="file-upload" type="file" accept="image/*"/>
+  </div>*/}
     
 
-    <div className="profile-pic">
+  <div className="profile-pic">
         <img src={user} alt="user-pic"/>
-        </div>
+  </div>
 
 <div className="user-infos">
     <h2>Name Surname</h2>
@@ -30,6 +67,8 @@ const UserCard = () => {
         </div>
 
         <Link to="/workRemote" className="single-activity">  <button>Work remote</button> </Link>
+
+        <Link to="/loginForm"> <button className="btnLogOut">Log out</button></Link>
     </div>
     
     
